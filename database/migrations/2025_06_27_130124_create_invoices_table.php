9bc2c3413bef1093c1dc81ef80fce84accb70726
Suhendra <sayasuhendra@gmail.com>
2025-06-27 20:29:03 +0700

    added migrations

--- database/migrations/2025_06_27_130124_create_invoices_table.php
+++ database/migrations/2025_06_27_130124_create_invoices_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,12 @@ return new class extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('number');
+            $table->string('date');
+            $table->string('name');
+            $table->string('total');
+            $table->boolean('verified')->default(0); // 0 unverified, 1 verified
+            $table->text('notes')->nullable();
         });
     }
 
